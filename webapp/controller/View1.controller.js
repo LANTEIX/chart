aa8e5365cd729294235ed0749bdf8841ed4d487c
Luiz Antonio <lanteix@gmail.com>
2019-09-20 22:48:03 +0000

    Versão 1 do grafico com Js

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,10 +1,27 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
 	"use strict";
 
-	return Controller.extend("ovly.chart.controller.View1", {
+	return Controller.extend("ovly.chart.controller.S0", {
 		onInit: function () {
+			this.oChartModel = new JSONModel({
+				title: {
+					text: 'Vendas por ano',
+					style: {
+						color: 'orange'
+					}
+				},
+				legendGroup: {
+					layout: {
+						position: "bottom",
+						alignment: "center"
+					}
+				}
+			});
+			
+			this.getView().setModel(this.oChartModel, "chart");
 
 		}
 	});
